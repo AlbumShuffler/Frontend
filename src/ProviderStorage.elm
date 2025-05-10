dda818d54f376d382fad1d70f25f458aa0338723
b0wter <b0wter@gmx.de>
2025-05-10 07:21:12 +0200

    Removed ProviderStorage elm since it will be auto-generated

--- src/ProviderStorage.elm
+++
@@ -1,8 +0,0 @@
-module ProviderStorage exposing(providers)
-import Providers
-
-providers : List Providers.Provider
-providers =
-    [ { name = "spotify", icon = "img/spotify.svg" }
-    , { name = "deezer", icon = "img/deezer.png" }
-    ]
